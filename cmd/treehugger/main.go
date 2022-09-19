0a807f4e8ad2bd106faa92826fd7f9e6a160ef40
Tom Hudson <thudson@bishopfox.com>
2022-09-19 14:29:03 +0100

    Switches treehugger to using jsluice lib

--- cmd/treehugger/main.go
+++ cmd/treehugger/main.go
@@ -5,20 +5,15 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 
-	sitter "github.com/smacker/go-tree-sitter"
-	"github.com/smacker/go-tree-sitter/javascript"
+	"github.com/bishopfoxmss/jsluice"
 )
 
 func main() {
 
 	flag.Parse()
 	queryStr := flag.Arg(0)
-
-	parser := sitter.NewParser()
-	parser.SetLanguage(javascript.GetLanguage())
 
 	sc := bufio.NewScanner(os.Stdin)
 	for sc.Scan() {
@@ -28,37 +23,12 @@ func main() {
 			continue
 		}
 
-		enter := func(n *sitter.Node) {
-			content := n.Content(source)
-			fmt.Println(content)
+		analyzer := jsluice.NewAnalyzer(source)
+
+		enter := func(n *jsluice.Node) {
+			fmt.Println(n.Content())
 		}
 
-		tree := parser.Parse(nil, source)
-		root := tree.RootNode()
-
-		query(root, queryStr, enter)
-	}
-}
-
-func query(n *sitter.Node, queryStr string, enter func(*sitter.Node)) {
-
-	q, err := sitter.NewQuery([]byte(queryStr), javascript.GetLanguage())
-	if err != nil {
-		log.Fatal(err)
-	}
-	qc := sitter.NewQueryCursor()
-	defer qc.Close()
-
-	qc.Exec(q, n)
-
-	for {
-		match, exists := qc.NextMatch()
-		if !exists || match == nil {
-			break
-		}
-
-		for _, capture := range match.Captures {
-			enter(capture.Node)
-		}
+		analyzer.Query(queryStr, enter)
 	}
 }
