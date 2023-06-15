3e9782140a51f1a3ef071e3ed8fd07958adc5265
Tom Hudson <thudson@bishopfox.com>
2023-06-15 16:54:25 +0100

    Adds secrets section to cmd readme

--- cmd/jsluice/testdata/simple-b64.js
+++ cmd/jsluice/testdata/simple-b64.js
@@ -1,6 +1,6 @@
 function getConfig(){
     let config = {
-        apiKey: "abc123xyz256",
+        randomStr: "abc123xyz256",
         secret: "I quite like PHP",
     }
     return "eyJsb2wiOiAic29tZSBKU09OISIsICJjb3VudCI6IDEyM30K"
